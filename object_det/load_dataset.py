980360a10053fef234ec9fb40496f0357f929cf5
Nerdy-Thanos <47348592+Nerdy-Thanos@users.noreply.github.com>
2022-06-05 02:32:04 +0100

    Update load_dataset.py

--- object_det/load_dataset.py
+++ object_det/load_dataset.py
@@ -1,4 +1,23 @@
 import torch
-from torchvision import datasets
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
 
-pascal_train = datasets.VOCSegmentation("pascal", year= "2012", image_set= "train", download=True)
+#resizing and converting to tensor
+transform = transforms.transforms.Compose([transforms.Resize(128),
+                                           transforms.ToTensor()])
+#loading the train set
+pascal_train = datasets.VOCSegmentation("pascal_train", year="2007", 
+                                         image_set="train", 
+                                         transforms= transform,
+                                         download=True)
+
+#Loading the test set
+pascal_test = datasets.VOCSegmentation("pascal_test", year = "2007",
+                                        image_set="test", 
+                                        transforms=transform, 
+                                        download=True)
+
+print(pascal_train.masks[0])
+#Loaders
+train_loader = DataLoader(pascal_train, batch_size=64)
+test_loader = DataLoader(pascal_test, batch_size=64)
