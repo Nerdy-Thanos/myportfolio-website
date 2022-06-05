b10ef016bd812b1f2bd6ed5f87be59cfa413f560
Nerdy-Thanos <47348592+Nerdy-Thanos@users.noreply.github.com>
2022-06-05 03:57:14 +0100

    Update load_dataset.py

--- object_det/load_dataset.py
+++ object_det/load_dataset.py
@@ -1,23 +1,27 @@
-import torch
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
-#resizing and converting to tensor
-transform = transforms.transforms.Compose([transforms.Resize(128),
-                                           transforms.ToTensor()])
-#loading the train set
-pascal_train = datasets.VOCSegmentation("pascal_train", year="2007", 
-                                         image_set="train", 
-                                         transforms= transform,
-                                         download=True)
+class Pascal(object):
+    def __init__(self) -> None:
+        pass
 
-#Loading the test set
-pascal_test = datasets.VOCSegmentation("pascal_test", year = "2007",
-                                        image_set="test", 
-                                        transforms=transform, 
-                                        download=True)
+    #Function to load and 
+    def get_data():
+        #resizing and converting to tensor
+        transform = transforms.transforms.Compose([transforms.Resize(128),
+                                                transforms.ToTensor()])
+        #loading the train set
+        pascal_train = datasets.VOCDetection("pascal_train", year="2007", 
+                                                image_set="train", 
+                                                transforms= transform,
+                                                download=True)
+        #Loading the test set
+        pascal_test = datasets.VOCDetection("pascal_test", year = "2007",
+                                                image_set="test", 
+                                                transforms=transform, 
+                                                download=True)
+        #Loaders
+        train_loader = DataLoader(pascal_train, batch_size=64)
+        test_loader = DataLoader(pascal_test, batch_size=64)
 
-print(pascal_train.masks[0])
-#Loaders
-train_loader = DataLoader(pascal_train, batch_size=64)
-test_loader = DataLoader(pascal_test, batch_size=64)
+        return train_loader, test_loader
