2d8164f0ae3c905ec52d6fd06e0e6052e469e289
Nerdy-Thanos <47348592+Nerdy-Thanos@users.noreply.github.com>
2022-06-05 05:56:11 +0100

    Update load_dataset.py

--- object_det/load_dataset.py
+++ object_det/load_dataset.py
@@ -1,6 +1,7 @@
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
+
 class Pascal(object):
     def __init__(self) -> None:
         pass
@@ -8,17 +9,17 @@ class Pascal(object):
     #Function to load and 
     def get_data():
         #resizing and converting to tensor
-        transform = transforms.transforms.Compose([transforms.Resize(128),
-                                                transforms.ToTensor()])
+        transform = transforms.Compose([transforms.Resize(128),
+                                        transforms.ToTensor()])
         #loading the train set
-        pascal_train = datasets.VOCDetection("pascal_train", year="2007", 
-                                                image_set="train", 
-                                                transforms= transform,
+        pascal_train = datasets.VOCSegmentation("pascal_train", year="2007", 
+                                                image_set="train",
+                                                transforms=transform, 
                                                 download=True)
         #Loading the test set
-        pascal_test = datasets.VOCDetection("pascal_test", year = "2007",
+        pascal_test = datasets.VOCSegmentation("pascal_test", year = "2007",
                                                 image_set="test", 
-                                                transforms=transform, 
+                                                transforms=transform,
                                                 download=True)
         #Loaders
         train_loader = DataLoader(pascal_train, batch_size=64)
