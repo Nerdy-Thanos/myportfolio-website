3527fd69af2e94c59ebb26aa03b5e3b5d5c9b100
Nerdy-Thanos <47348592+Nerdy-Thanos@users.noreply.github.com>
2022-06-05 02:01:55 +0100

    Update base_model.py

--- object_det/base_model.py
+++ object_det/base_model.py
@@ -1,7 +1,13 @@
 import torch
 from torch.nn import functional
 import platform
+from torchvision import datasets
 
 #Checking platform and device
 print(platform.platform())
 print(torch.has_mps)
+
+#Creating the model class
+class BaseModel(object):
+    def __init__(self) -> None:
+         
