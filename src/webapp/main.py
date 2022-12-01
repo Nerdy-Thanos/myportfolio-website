4356379026428d8f6d868f454209ebef21d2e2c8
viggy-21 <47348592+Nerdy-Thanos@users.noreply.github.com>
2022-12-01 16:25:58 +0000

    calculate filings

--- src/webapp/main.py
+++ src/webapp/main.py
@@ -1,7 +1,8 @@
-from src.companies_house.pdf_filings.fetch_pdf_filings import extract_filings_documents
-
+import random
+from ..companies_house.pdf_filings.fetch_pdf_filings import extract_filings_documents
+from ..companies_house.pdf_filings.random import random_company_list
 from . import create_app
-from flask import render_template
+from flask import render_template,request
 
 app = create_app()
 
@@ -11,9 +12,19 @@ def hello():
 
 @app.route("/ParseFilingsDocuments",methods=["GET","POST"])
 def ParseFilingsDocuments():
-    company_number = None
-    filings_data = extract_filings_documents(company_number)
+    if request.method=="POST":
+        company_number=None
+        if request.form["submit"]=="single":
+            company_number = request.form.get("cn")
+        if request.form["submit"]=="random":
+            company_number=random.choice(random_company_list)
+        filings_data = extract_filings_documents(company_number)
+        return render_template('filings_table.html',  tables=[filings_data.to_html(classes='data')], titles=filings_data.columns.values)
     return render_template("filings.html")
+
+@app.route("/DownloadFilingPDF")
+def DownloadFilingPDF():
+    return None
 
 if __name__=="__main__":
     app.run()
