c7dab47bd5e9fe499a24c5fba6c3d989b43560bd
viggy-21 <47348592+Nerdy-Thanos@users.noreply.github.com>
2022-12-01 12:40:46 +0000

    setup template main page

--- src/webapp/main.py
+++ src/webapp/main.py
@@ -1,10 +1,11 @@
 from . import create_app
+from flask import render_template
 
 app = create_app()
 
 @app.route("/")
 def hello():
-    return "Hello"
+    return render_template("index.html")
 
 if __name__=="__main__":
     app.run()
