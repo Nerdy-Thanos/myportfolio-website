a0b7e55a1f7fb97f885db6434d8a852124dffd7a
viggy-21 <47348592+Nerdy-Thanos@users.noreply.github.com>
2022-12-12 14:11:01 +0000

    add clientside

--- src/webapp/main.py
+++ src/webapp/main.py
@@ -1,19 +1,38 @@
+import base64
+from io import StringIO
+import io
 import random
-
-from src.face_mask.test import recognise_mask
+from PIL import Image
+from src.face_mask.camera import Camera
+from src.face_mask.camera_flip import Flip
+from numpy import array
+from ..face_mask.test import load_models, recognise_mask
 from ..song_bot.fetch_data import make_dataset
 from ..song_bot.load_and_predict import load_saved_model, predict_next_words
-
+from sys import stdout
 from ..song_bot.train import model_architecture, train_model
-
+import logging
 from .helper_functions import clean_output
 from ..companies_house.pdf_filings.fetch_pdf_filings import extract_filings_documents
 from ..companies_house.pdf_filings.random import random_company_list
 from . import create_app
 from flask import render_template,request, send_from_directory, redirect, url_for, Response
-
+import cv2
+import time
+from imutils.video import VideoStream
+from flask_socketio import SocketIO, emit
 
 app = create_app()
+
+app.logger.addHandler(logging.StreamHandler(stdout))
+app.config['SECRET_KEY'] = 'secret!'
+app.config['DEBUG'] = True
+socketio = SocketIO(app)
+camera = Camera(Flip())
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+	app.logger.info("client connected")
 
 @app.route("/")
 def hello():
@@ -69,18 +88,29 @@ def SongBotPredict(artist):
 def FaceMasks():
 	return render_template("mask.html")
 
-@app.route('/DetectMask')
-def DetectMask():
-	"""Video streaming route. Put this in the src attribute of an img tag."""
-	return Response(gen_frame(),
-					mimetype='multipart/x-mixed-replace; boundary=frame')
+@socketio.on('image')
+def image(data_image):
+	sbuf = StringIO()
+	sbuf.write(data_image)
 
-def gen_frame():
-	"""Video streaming generator function."""
-	while True:
-		frame = recognise_mask()
-		yield (b'--frame\r\n'
-			   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+	# decode and convert into image
+	b = io.BytesIO(base64.b64decode(data_image))
+	pimg = Image.open(b)
+
+	## converting RGB to BGR, as opencv standards
+	frame = cv2.cvtColor(array(pimg), cv2.COLOR_RGB2BGR)
+	## converting RGB to BGR, as opencv standards
+	face_detection_model, mask_detection_model = load_models()
+
+	output_frame = recognise_mask(frame, face_detection_model, mask_detection_model)
+
+	# base64 encode
+	stringData = base64.b64encode(output_frame).decode('utf-8')
+	b64_src = 'data:image/jpg;base64,'
+	stringData = b64_src + stringData
+
+	# emit the frame back
+	emit('response_back', stringData)
 
 if __name__=="__main__":
-	app.run()
+	socketio.run()
